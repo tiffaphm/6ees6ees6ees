ead9fdf9235535d815f9c7dec75fa0f133d20979
Tiffany Pham <tiffanynpham@gmail.com>
2017-08-29 21:31:47 -0700

    Passed all tests for implementing bees classes in ES6

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,16 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor(age, color, food, job, honeyPot) {
+    super(food, color)
+    this.age = 10
+    this.job = 'make honey'
+    this.honeyPot = 0
+  }
+
+  makeHoney() {
+    this.honeyPot++
+  }
+
+  giveHoney() {
+    this.honeyPot--
+  }
 };
